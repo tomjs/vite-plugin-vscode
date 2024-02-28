919939c7c59f2fb3d66cd4cc93b529bca7a8c611
Tom Gao <tom@tomgao.cc>
2024-02-28 22:10:02 +0800

    refactor: modify sample code

--- examples/vue/extension/views/helper.ts
+++ examples/vue/extension/views/helper.ts
@@ -2,10 +2,9 @@ import { Disposable, ExtensionContext, Webview, window } from 'vscode';
 
 export class WebviewHelper {
   public static setupHtml(webview: Webview, context: ExtensionContext) {
-    if (process.env.VITE_DEV_SERVER_URL) {
-      return __getWebviewHtml__(process.env.VITE_DEV_SERVER_URL);
-    }
-    return __getWebviewHtml__(webview, context);
+    return process.env.VITE_DEV_SERVER_URL
+      ? __getWebviewHtml__(process.env.VITE_DEV_SERVER_URL)
+      : __getWebviewHtml__(webview, context);
   }
 
   public static setupWebviewHooks(webview: Webview, disposables: Disposable[]) {
