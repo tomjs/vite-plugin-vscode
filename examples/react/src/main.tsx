f882fca22cbc850f6ccdabf1dc526879cac6b0bb
Tom Gao <tom@tomgao.cc>
2024-06-27 00:10:50 +0800

    chore: remove @tomjs/vscode-extension-webview package

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line simple-import-sort/imports
-import '@tomjs/vscode-extension-webview/client';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
